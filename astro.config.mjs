756c113f97bab59235507b650755985bf4c793d7
Naveen Kumar <naveen.kottidi@springfieldca.org>
2026-06-08 00:39:08 +0530

    Setup Astro deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.com'
+  // Replace with your GitHub Pages URL
+  site: 'https://naveenkumar-s563104.github.io',
+  base: '/AI-School',
 });
